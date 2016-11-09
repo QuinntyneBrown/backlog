eb2f3c98e094d0f236fa872ce84ac77d774ee7ca
Quinntyne Brown <quinntynebrown@gmail.com>
2016-11-08 20:22:04 -0500

    drop-down

---
+++ Backlog.Web.V2/src/app/shared/drop-down.component.ts
@@ -0,0 +1,39 @@
+let template = require("./drop-down.component.html");
+let styles = require("./drop-down.component.scss");
+
+export class DropDownComponent extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    static get observedAttributes () {
+        return [];
+    }
+
+    connectedCallback() {
+        this.innerHTML = `<style>${styles}</style> ${template}`;
+        this._bind();
+        this._addEventListeners();
+    }
+
+    private _bind() {
+
+    }
+
+    private _addEventListeners() {
+
+    }
+
+    disconnectedCallback() {
+
+    }
+
+    attributeChangedCallback (name, oldValue, newValue) {
+        switch (name) {
+            default:
+                break;
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded",() => window.customElements.define(`ce-drop-down`,DropDownComponent));
