cab76e445625bbd162c44c03bd636d84423a6b94
Quinntyne Brown <quinntynebrown@gmail.com>
2017-02-28 05:00:19 -0500

    use DOMParser in create-element

--- src/Backlog.Frontend/src/app/utilities/create-element.ts
+++ src/Backlog.Frontend/src/app/utilities/create-element.ts
@@ -1,5 +1,2 @@
-﻿export const createElement = (html:string): HTMLElement => {
-    let divElement = document.createElement("div")
-    divElement.innerHTML = html;   
-    return divElement.firstChild as HTMLElement;
-}
+﻿export const createElement = (html: string): HTMLElement =>
+    new DOMParser().parseFromString(html, "text/html").body.firstChild as HTMLElement;
