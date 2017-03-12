91e8208f1d5b85fbdd6eca8cf6963065e14a5166
Quinntyne Brown <quinntynebrown@gmail.com>
2017-03-12 06:35:51 -0400

    spaces

--- src/Backlog/Features/Epics/GetEpicByIdQuery.cs
+++ src/Backlog/Features/Epics/GetEpicByIdQuery.cs
@@ -8,40 +8,40 @@ using System.Data.Entity;
 
 namespace Backlog.Features.Epics
 {
-	public class GetEpicByIdQuery
-	{
-		public class GetEpicByIdRequest : IRequest<GetEpicByIdResponse> { 
-			public int Id { get; set; }
-		}
+    public class GetEpicByIdQuery
+    {
+        public class GetEpicByIdRequest : IRequest<GetEpicByIdResponse> { 
+            public int Id { get; set; }
+        }
 
-		public class GetEpicByIdResponse
-		{
-			public EpicApiModel Epic { get; set; } 
-		}
+        public class GetEpicByIdResponse
+        {
+            public EpicApiModel Epic { get; set; } 
+        }
 
-		public class GetEpicByIdHandler : IAsyncRequestHandler<GetEpicByIdRequest, GetEpicByIdResponse>
-		{
-			public GetEpicByIdHandler(IBacklogContext context, ICache cache)
-			{
-				_context = context;
-				_cache = cache;
-			}
+        public class GetEpicByIdHandler : IAsyncRequestHandler<GetEpicByIdRequest, GetEpicByIdResponse>
+        {
+            public GetEpicByIdHandler(IBacklogContext context, ICache cache)
+            {
+                _context = context;
+                _cache = cache;
+            }
 
-			public async Task<GetEpicByIdResponse> Handle(GetEpicByIdRequest request)
-			{                
-				return new GetEpicByIdResponse()
-				{
-					Epic = EpicApiModel.FromEpic(await _context.Epics
-					.Include(x => x.Stories)
-					.Include(x => x.Product)
-					.SingleAsync(x => x.Id == request.Id))
-				};
-			}
+            public async Task<GetEpicByIdResponse> Handle(GetEpicByIdRequest request)
+            {                
+                return new GetEpicByIdResponse()
+                {
+                    Epic = EpicApiModel.FromEpic(await _context.Epics
+                    .Include(x => x.Stories)
+                    .Include(x => x.Product)
+                    .SingleAsync(x => x.Id == request.Id))
+                };
+            }
 
-			private readonly IBacklogContext _context;
-			private readonly ICache _cache;
-		}
+            private readonly IBacklogContext _context;
+            private readonly ICache _cache;
+        }
 
-	}
+    }
 
 }
